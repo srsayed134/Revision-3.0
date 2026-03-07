cf1556682798814c86eb267f0096db5af692358f
srsayed134 <srsayed134@gmail.com>
2026-03-07 06:49:07 +0600

    Pythond inter., func, var, comment

--- hello.py
+++ hello.py
@@ -1,1 +1,37 @@
+# 1. Start python
+"""
 print("HEllo, World")
+"""
+
+# 2. Understand bug
+"""
+print("Test" -> error
+"""
+
+# 3. Variables
+"""
+name = input("What's your name?")
+print("hello,")
+print(name);
+"""
+
+# 4. Comment (Show an example how comment can work in pseudocode)
+
+"""
+#Ask user for name
+name = input("Hey, what is your name? ")
+# Say hello to user
+print("Hello");
+print(name)
+""" 
+# 5. Printing line in one 
+"""
+name = input("What is your name ");
+print("Hello, " + name) # it concate 
+"""
+"""
+name = input("What is your name? ");
+print("Hello,", name) # in two arguments python add a space 
+"""
+
+
