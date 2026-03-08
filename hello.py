05b5ec3be22960c5f73e6281a5f90a9584992926
srsayed134 <srsayed134@gmail.com>
2026-03-08 12:02:09 +0600

    Named para., escape car., f-string, string method

--- hello.py
+++ hello.py
@@ -33,5 +33,50 @@ print("Hello, " + name) # it concate
 name = input("What is your name? ");
 print("Hello,", name) # in two arguments python add a space 
 """
+# 6. Named Prarmeters
+
+"""
+name = input("What's your name?")
+print("hello, ", end="") #because of end="" default end="\n" not work
+print(name)
+"""
+
+"""
+name = input("What's your name? ")
+print("hello,", name, sep="???") #hello,???David  #wehn i use sep="???" 
+"""
+
+#7. Escaping caracter
+"""
+print("Hello, \"friend\"")
+"""
+
+#8.f-string
+"""
+name = input("What is your name? ") 
+print(f"hello, {name}") #in there name consider as dynamicly
+"""
+
+# 9. String method
+"""
+name = input("What is your name ? ")
+name = name.strip() # Remove whitespacce from str
+# name = name.capitalize() #Capitalize first word
+name = name.title() #It isuse for name all word's first later will capital
+print(f"hello, {name}")
+"""
+
+# also
+"""
+name = input("What is your name? ")
+name = name.strip().title()
+print(f"hello, {name}")
+"""
+
+# also 
+
+name = input("What is your name? ").strip().title()
+print(f"Hello, {name}")
+
 
 
